4112c7fbe3d651754f496a3380e185ea23e50d3d
ash <97464181+Borgerr@users.noreply.github.com>
2024-06-07 17:27:34 -0700

    update, read

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,35 @@ async fn create(cred: &Cred, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> 
     Ok(())
 }
 
+async fn update(
+    cred: &Cred, website: &str, pool: &sqlx::PgPool
+) -> Result<(), Box<dyn Error>> {
+    let query = "UPDATE creds SET usr = $1, pass = $2 WHERE website = $3";
+
+    sqlx::query(query)
+        .bind(&cred.usr)
+        .bind(&cred.pass)
+        .bind(&cred.website)
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
+
+async fn read(pool: &sqlx::PgPool) -> Result<Cred, Box<dyn Error>> {
+    let q_str = "SELECT usr, pass, website FROM creds";
+    let query = sqlx::query(q_str);
+
+    let row = query.fetch_one(pool).await?;
+    let cred = Cred{
+        usr: row.get("usr"),
+        pass: row.get("pass"),
+        website: row.get("website"),
+    };
+
+    Ok(cred)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut args = std::env::args();
